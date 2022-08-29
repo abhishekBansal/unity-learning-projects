a95aa7fbfd0dc510511546fc37485ccfbd5df7c1
Abhishek Bansal <discover.ab@gmail.com>
2022-08-29 16:31:54 +0530

    + data persistence

--- Warehouse-SceneFlow/Assets/Scripts/MainManager.cs
+++ Warehouse-SceneFlow/Assets/Scripts/MainManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -18,5 +19,36 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            teamColor = data.teamColor;
+        }
+        else {
+            Debug.Log("Save file doesn't exists");
+        }
     }
 }
